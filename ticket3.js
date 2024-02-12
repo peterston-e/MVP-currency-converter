96714a968a57322c43854403f2f8a751ec0dcd22
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-02-12 19:46:42 +0000

    copied code from fork

---
+++ ticket3.js
@@ -0,0 +1,23 @@
+var form = document.getElementById("forex-converter");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let usdConversion = 1.27;
+  let eurConversion = 1.17;
+  let gbp = document.querySelector("#gbp").value;
+  let forex = document.getElementById("fcurrency").value;
+
+  if (forex === "usd") {
+    let conversion = gbp * usdConversion;
+
+    document.getElementById(
+      "conversion"
+    ).textContent = `£${gbp} = $${conversion}`;
+  } else if (forex === "eur") {
+    let conversion = gbp * eurConversion;
+
+    document.getElementById(
+      "conversion"
+    ).textContent = `£${gbp} = $${conversion}`;
+  }
+});
